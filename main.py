6a41a38818d0a106e0eec22475dcdb821d62fd81
reagan294 <jfullerton@nic.edu>
2025-02-19 19:15:10 +0000

    Added to main.py examples on how to code in Python

---
+++ main.py
@@ -0,0 +1,32 @@
+print("Hello World!")
+
+my_variable = 6
+num_cats = 8
+
+print(num_cats)
+
+print("num cats: ", num_cats)
+print(f"number of cats {num_cats}")
+print(f"my age {my_variable} and my cats {num_cats}")
+
+# answer = input("What is your name?") --> # means comment
+
+x = 6
+y = 20
+print(f"x={x}")
+c = x + y
+print(c)
+
+print(f"c = {x+y}")
+print(f"x*y = {x*y}")
+# x**y means x to the power of y
+
+"""
+This is a multi-line comment
+Holla yo
+"""
+
+num1 = input("What is the first number?")
+num2 = input("What is the second number?")
+sum = int(num1) + int(num2)
+print(f"{num1} + {num2} = {sum}")
